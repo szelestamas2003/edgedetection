07d09a31028cdcd5a6a2ef7f41a2110a523721f4
J2868O <j2868o@inf.elte.hu>
2024-05-05 19:54:27 +0200

    Modified Qt version to Qt 6.7.0

--- EdgeDetection/EdgeDetection/main.cpp
+++ EdgeDetection/EdgeDetection/main.cpp
@@ -1,9 +1,11 @@
 #include "EdgeDetection.h"
 #include <QtWidgets/QApplication>
+#include "qstylefactory.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("windowsvista"));
     EdgeDetection w;
     w.show();
     return a.exec();
